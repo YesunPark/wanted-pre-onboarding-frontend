e61a8fb52a688926cf2ce0a7c618b39e28511f77
YesunPark <lynn08082@gmail.com>
2022-12-11 19:53:51 +0900

    Add: global style 추가

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
+import GlobalStyle from './styles/global-styles';
 import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
